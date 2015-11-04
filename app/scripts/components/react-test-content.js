bccb96e3e41770ed61aed73aa96bc8ee1a77c808
Dejan Tasic <dtasic@gmail.com>
2015-11-04 16:43:11 +0100

    update

--- app/scripts/components/react-test-content.js
+++ app/scripts/components/react-test-content.js
@@ -1,34 +1,3 @@
-/*
-
-CSS:
-
-#modal {
-  position: fixed;
-  top: 0;
-  bottom: 0;
-  right: 0;
-  left: 0;
-  z-index: 9000;
-  background: rgba(0,0,0,0.8);
-}
-
-.modal-close {
-  cursor: pointer;
-  color: white;
-  padding: 20px;
-  z-index: 9100;
-  position: absolute;
-  top: 0;
-  right: 0;
-}
-
-modal-close:hover {
-  color: red;
-}
-
-
-*/
-
 import React from 'react';
 
 export default class Test extends React.Component {
@@ -38,9 +7,8 @@ export default class Test extends React.Component {
   }
 
   render() {
-	var democontent = <span id='content'>some other componenet as content for modal 2 passed via prop</span>;
     return (
-		<div>{democontent}</div>
+		<span id='content'>some other componenet as content for modal 2 passed via prop with {this.props.someprop}</span>
     );
   }
 }
